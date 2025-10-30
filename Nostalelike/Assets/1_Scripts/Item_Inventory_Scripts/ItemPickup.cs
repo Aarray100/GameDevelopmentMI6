0eec3e00a530f86d214e502eeabb3b9949bd24b6
Aarray100 <aaronseitz.bw@icloud.com>
2025-10-30 21:18:00 +0100

    Itemprefab zum pickup gemacht, als naechstes dann noch hotbar, und drag and drop fuer inventory sowohl als auch die hotbar wenn das inventar geoffnet ist

--- Nostalelike/Assets/1_Scripts/Item_Inventory_Scripts/ItemPickup.cs
+++ Nostalelike/Assets/1_Scripts/Item_Inventory_Scripts/ItemPickup.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class ItemPickup : MonoBehaviour
 {
-   
+
     public ItemData itemToPickup;
     public int quantity = 1;
+
+    private SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (itemToPickup != null && itemToPickup.itemIcon != null)
+        {
+            spriteRenderer.sprite = itemToPickup.itemIcon;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
